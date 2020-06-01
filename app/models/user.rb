f1dfa118140a7806a7f08a0fe4e31234d9344fb3
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-06-01 14:20:37 +0300

    update create method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :username, presence: true,
-                   length: { :within 3..20 }
+                   length: { minimum: 3, maximum: 20 }
                 
     before_save { email.downcase! }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,6 +8,6 @@ class User < ApplicationRecord
                 format: { with: VALID_EMAIL_REGEX },
                 uniqueness: true
 
-    VALID_PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
-    validates :password, presence: true,  format: { with: VALID_PASSWORD_REGEX }
+    # VALID_PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
+    validates :password, presence: true,  length: {maximum: 20}
 end
