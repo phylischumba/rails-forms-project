2345d05cc5643626d87dc813139bb078a38573eb
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-06-01 20:06:39 +0300

    adds linter files and fix errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,45 +1,44 @@
 class UsersController < ApplicationController
-    def index 
-      @users = User.all
-    end
+  def index
+    @users = User.all
+  end
 
-    def show
-      @user = User.find(params[:id])
-    end
+  def show
+    @user = User.find(params[:id])
+  end
 
-    def new
-        @user = User.new
-    end
+  def new
+    @user = User.new
+  end
 
-    def create
-      @user = User.new(user_params)
-      @user.save
-      # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-      
+  def create
+    @user = User.new(user_params)
+    @user.save
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
-      if @user.save
-        flash.notice = "User '#{@user.username}' Created!"
-        redirect_to users_path
-      else
-       flash.notice = "User '#{@user.username}' Not Created!"
-        render :new
-      end
-    end
-
-    def edit
-      @user = User.find(params[:id])
-    end
-
-    def update
-      @user = User.find(params[:id])
-      @user.update(user_params)
-
+    if @user.save
+      flash.notice = "User '#{@user.username}' Created!"
       redirect_to users_path
+    else
+      flash.notice = "User '#{@user.username}' Not Created!"
+      render :new
     end
+  end
 
-    private
-    
-    def user_params
-      params.require(:user).permit(:username, :email, :password)
-    end
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-    validates :username, presence: true,
-                   length: { minimum: 3, maximum: 20 }
-                
-    before_save { email.downcase! }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 255 },
-                format: { with: VALID_EMAIL_REGEX },
-                uniqueness: true
+  validates :username, presence: true,
+                       length: { minimum: 3, maximum: 20 }
 
-    validates :password, presence: true, length: { minimum: 8, maximum: 25 }
+  before_save { email.downcase! }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+
+  validates :password, presence: true, length: { minimum: 8, maximum: 25 }
 end
